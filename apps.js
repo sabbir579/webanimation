58adb7cee693cb78b0217dceb581e92feb179039
Sabbir Hosen <rsabbirhossain.official@gmail.com>
2024-10-27 05:33:26 +0600

    animation commit

---
+++ apps.js
@@ -0,0 +1,13 @@
+var btn1=document.getElementById("btn1")
+var pic=document.getElementById("pic")
+
+
+function btnn(){
+    h11.style.color="red"
+    btn1.style.backgroundColor="black"
+    btn1.style.width="200px"
+    pic.style.display="none"
+    pic2.style.display="block"
+
+}
+
